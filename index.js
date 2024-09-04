89cfdb0892d7c25e39078a569e146d1c42ec2235
Zphinx.GZ <zeeyou.ou@gmail.com>
2024-09-04 16:13:37 +0700

    WellDone

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 console.log("Hello World!!");
 
-
-//แสดงผล
-
+//แสดงผลตาราง
 document.addEventListener("DOMContentLoaded", () => {
+  // Store
+  document.getElementById("nameInvoice").innerHTML = dataStore[0].nameInvoice;
+  document.getElementById("addressInvoice").innerHTML = dataStore[0].addressInvoice;
+  document.getElementById("cityInvoice").innerHTML = dataStore[0].cityInvoice;
+
+  // Customer Billto
+  let nameOfBillTo = document.getElementById("nameBill");
+  let addressBillTo = document.getElementById("addressBill");
+  let cityBillTo = document.getElementById("cityBill");
+  nameOfBillTo.innerHTML = dataCustomer[0].nameCustomerBill;
+  addressBillTo.innerHTML = dataCustomer[0].addressBillTo;
+  cityBillTo.innerHTML = dataCustomer[0].cityBillTo;
+
+  // Customer Shipto
+  let nameOfShipTo = document.getElementById("nameShip");
+  let addressShipTo = document.getElementById("addressShip");
+  let cityShipTo = document.getElementById("cityShip");
+  nameOfShipTo.innerHTML = dataCustomer[0].nameCustomerShip;
+  addressShipTo.innerHTML = dataCustomer[0].addressShipTo;
+  cityShipTo.innerHTML = dataCustomer[0].cityShipTo;
+
+  // invoice data
+  let invoiceText = document.getElementById("invoiceText");
+  let invoiceDate = document.getElementById("invoiceDate");
+  let poNumber = document.getElementById("poNumber");
+  let dueDate = document.getElementById("dueDate");
+  invoiceText.innerHTML = invoiceData[0].invoiceText;
+  invoiceDate.innerHTML = invoiceData[0].invoiceDate;
+  poNumber.innerHTML = invoiceData[0].poNumber;
+  dueDate.innerHTML = invoiceData[0].dueDate;
+
   let tbody = document.getElementById("itemsToPort");
   let subtotal = 0;
-
   //แสดงผลตาราง
   items.forEach((item) => {
     let row = document.createElement("tr");
@@ -41,38 +69,6 @@ document.addEventListener("DOMContentLoaded", () => {
   let totalRow = document.createElement("tr");
   totalRow.innerHTML = `
             <td colspan="3" style="font-weight: bold; font-size: 20px;" class="totalMain">TOTAL</td>
-            <td class="totalAmount">$${total.toFixed(2)}</td>
-        `;
+            <td class="totalAmount">$${total.toFixed(2)}</td>`;
   tbody.appendChild(totalRow);
-
-  // Store
-  document.getElementById("nameInvoice").innerHTML = dataStore[0].nameInvoice;
-  document.getElementById("addressInvoice").innerHTML =
-    dataStore[0].addressInvoice;
-  document.getElementById("cityInvoice").innerHTML = dataStore[0].cityInvoice;
-  // Customer Billto
-  let nameOfBillTo = document.getElementById("nameBill");
-  let addressBillTo = document.getElementById("addressBill");
-  let cityBillTo = document.getElementById("cityBill");
-  nameOfBillTo.innerHTML = dataCustomer[0].nameCustomerBill;
-  addressBillTo.innerHTML = dataCustomer[0].addressBillTo;
-  cityBillTo.innerHTML = dataCustomer[0].cityBillTo;
-
-  // Customer Shipto
-  let nameOfShipTo = document.getElementById("nameShip");
-  let addressShipTo = document.getElementById("addressShip");
-  let cityShipTo = document.getElementById("cityShip");
-  nameOfShipTo.innerHTML = dataCustomer[0].nameCustomerShip;
-  addressShipTo.innerHTML = dataCustomer[0].addressShipTo;
-  cityShipTo.innerHTML = dataCustomer[0].cityShipTo;
-
-  // invoice data
-  let invoiceText = document.getElementById("invoiceText");
-  let invoiceDate = document.getElementById("invoiceDate");
-  let poNumber = document.getElementById("poNumber");
-  let dueDate = document.getElementById("dueDate");
-  invoiceText.innerHTML = invoiceData[0].invoiceText;
-  invoiceDate.innerHTML = invoiceData[0].invoiceDate;
-  poNumber.innerHTML = invoiceData[0].poNumber;
-  dueDate.innerHTML = invoiceData[0].dueDate;
 });
